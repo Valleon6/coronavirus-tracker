ed347fe2cb087f60d914bb7d8620119a72c45758
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-19 21:25:47 +0100

    Added an algorithm that retrieves parts of the data
    Implemeted a scheduler algorithm that reruns and updates the data on a scheduled timely basis

--- src/main/java/com/valleon/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/valleon/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,9 +1,15 @@
 package com.valleon.coronavirustracker.services;
 
 import jakarta.annotation.PostConstruct;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -15,6 +21,7 @@ public class CoronaVirusDataService {
     private static String VIRUS_DATA_URL= "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -22,7 +29,16 @@ public class CoronaVirusDataService {
                 .build();
 
        HttpResponse<String> httpResponse =  client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.body());
+//        System.out.println(httpResponse.body());
+
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+//            String customerNo = record.get("CustomerNo");
+//            String name = record.get("Name");
+            System.out.println(state);
+        }
     }
 
 }
